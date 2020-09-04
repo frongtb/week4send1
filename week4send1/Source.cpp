5a0ac17f4e773ab23ed90116c2b4320518fe7c48
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-09-04 16:13:32 +0700

    Add project files.

---
+++ week4send1/Source.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if (num >= 0)
+    {
+        if (num % 2 == 0)
+        {
+            printf("sqrt2 = %f", sqrt(num));
+            return 0;
+        }
+        else
+        {
+            printf("power = %.0f", pow(num, 2));
+            return 0;
+        }
+    }
+    return 0;
+}
